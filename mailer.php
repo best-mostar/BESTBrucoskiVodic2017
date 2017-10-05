ba983b340f546d07d8e0706a7f3da38b9969cf9e
mirza.krzic <mirza.krzic@best.eu.org>
2017-10-05 17:48:51 +0200

    Mailer_Alerts

---
+++ mailer.php
@@ -0,0 +1,25 @@
+<?php
+    function phpAlert($msg) {
+        echo '<script type="text/javascript">alert("' . $msg . '")</script>';
+    }
+
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        $name = $_POST['name'];
+        $msg = $_POST['message'];
+        $cName = $_POST['companyName'];
+        $mail = $_POST['mail'];
+
+        $subject = "[BEST Mostar][BV] Contact form";
+        $message = "Ime:" . $name . "\r\nCompany: " . $cName . "\r\nEmail: " . $mail . "\r\nPoruka: " . $msg;
+        $headers = "From: no-reply@best-mostar.org" . "\r\n" .
+            "Reply-To: no-reply@best-mostar.org" . "\r\n" .
+            "X-Mailer: PHP/" . phpversion();
+        mail('armin.comic@gmail.com', $subject, $message, $headers);
+
+        phpAlert("Your email has been sent!");
+
+    }
+    else
+        phpAlert("Something went wrong. Please try again!");
+        die();
+?>
